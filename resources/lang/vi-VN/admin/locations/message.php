84aab8bed98ed17745e6d3dec26fb1f4821df2da
dothanhbn84 <dothanhbn84@gmail.com>
2025-07-28 10:24:27 +0700

    Updated Vietnamese language

--- resources/lang/vi-VN/admin/locations/message.php
+++ resources/lang/vi-VN/admin/locations/message.php
@@ -2,34 +2,34 @@
 
 return array(
 
-    'does_not_exist' => 'Địa phương không tồn tại.',
+    'does_not_exist' => 'Vị trí không tồn tại.',
     'assoc_users'    => 'This location is not currently deletable because it is the location of record for at least one asset or user, has assets assigned to it, or is the parent location of another location. Please update your records to no longer reference this location and try again ',
-    'assoc_assets'	 => 'Địa phương này hiện tại đã được liên kết với ít nhất một tài sản và không thể xóa. Xin vui lòng cập nhật tài sản của bạn để không còn liên kết với địa phương này nữa và thử lại. ',
-    'assoc_child_loc'	 => 'Địa phương này hiện tại là cấp parent của ít nhật một địa phương con và không thể xóa. Xin vui lòng cập nhật địa phương của bạn để không liên kết đến địa phương này và thử lại. ',
+    'assoc_assets'	 => 'Vị trí này hiện tại đã được liên kết với ít nhất một tài sản và không thể xóa. Xin vui lòng cập nhật tài sản của bạn để không còn liên kết với Vị trí này nữa và thử lại. ',
+    'assoc_child_loc'	 => 'Vị trí này hiện tại là cấp parent của ít nhật một Vị trí con và không thể xóa. Xin vui lòng cập nhật Vị trí của bạn để không liên kết đến Vị trí này và thử lại. ',
     'assigned_assets' => 'Tài sản được giao',
     'current_location' => 'Vị trí hiện tại',
     'open_map' => 'Open in :map_provider_icon Maps',
 
 
     'create' => array(
-        'error'   => 'Địa phương chưa tạo, xin vui lòng thử lại.',
-        'success' => 'Địa phương đã tạo thành công.'
+        'error'   => 'Vị trí chưa tạo, xin vui lòng thử lại.',
+        'success' => 'Vị trí đã tạo thành công.'
     ),
 
     'update' => array(
-        'error'   => 'Địa phương chưa cập nhật, xin vui lòng thử lại',
-        'success' => 'Địa phương đã cập nhật thành công.'
+        'error'   => 'Vị trí chưa cập nhật, xin vui lòng thử lại',
+        'success' => 'Vị trí đã cập nhật thành công.'
     ),
 
     'restore' => array(
-        'error'   => 'Location was not restored, please try again',
-        'success' => 'Location restored successfully.'
+        'error'   => 'Vị trí không được khôi phục, hãy thử lại',
+        'success' => 'Vịt trí đã được khôi phục.'
     ),
 
     'delete' => array(
-        'confirm'   	=> 'Bạn có chắc muốn xóa địa phương này?',
-        'error'   => 'Có vấn đề xảy ra khi xóa địa phương. Xin vui lòng thử lại.',
-        'success' => 'Địa phương đã xóa thành công.'
+        'confirm'   	=> 'Bạn có chắc muốn xóa Vị trí này?',
+        'error'   => 'Có vấn đề xảy ra khi xóa Vị trí. Xin vui lòng thử lại.',
+        'success' => 'Vị trí đã xóa thành công.'
     )
 
 );
