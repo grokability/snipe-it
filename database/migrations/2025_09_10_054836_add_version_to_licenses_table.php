76ab8a1d8bf94717e43e8ce6d59095f35f4878e4
Nathan Taylor <ntaylor0377@gmail.com>
2025-09-10 16:09:39 +1000

    added down method for migration

--- database/migrations/2025_09_10_054836_add_version_to_licenses_table.php
+++ database/migrations/2025_09_10_054836_add_version_to_licenses_table.php
@@ -24,7 +24,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('licenses', function (Blueprint $table) {
-            //
+            $table->dropColumn('version');
         });
     }
 };
