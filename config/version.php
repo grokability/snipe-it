6fd90f6d5bcdd80179aeeb03af6516060ae30693
snipe <snipe@snipe.net>
2026-05-22 12:05:57 +0100

    Bumped hash and added pre-version

--- config/version.php
+++ config/version.php
@@ -1,11 +1,11 @@
 <?php
 
-return [
+return array(
     'app_version' => 'v8.5.1-pre',
-    'full_app_version' => 'v8.5.1-pre - build 22809-g86245ad4ae',
-    'build_version' => '22809',
+    'full_app_version' => 'v8.5.1-pre - build 22836-gc94fce2367',
+    'build_version' => '22836',
     'prerelease_version' => '',
-    'hash_version' => 'g86245ad4ae',
-    'full_hash' => 'v8.5.1-pre-185-g86245ad4ae',
+    'hash_version' => 'gc94fce2367',
+    'full_hash' => 'v8.5.1-pre-177-gc94fce2367',
     'branch' => 'develop',
-];
+);
